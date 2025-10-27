55fbdb25ea16bf510c46e4adbb080add1d6217f2
Stephan <B748@users.noreply.github.com>
2025-10-27 16:11:32 +0100

    add standard logging to bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+  const logger = new Logger('Bootstrap');
+
   // Enable CORS for SSE
   app.enableCors({
     origin: true,
@@ -13,7 +15,7 @@ async function bootstrap() {
   const port = process.env.PORT || 8600;
   await app.listen(port);
   
-  console.log(`Tesseract-API running on port ${port}`);
+  logger.log(`Tesseract-API running on port ${port}`);
 }
 
 void bootstrap();
